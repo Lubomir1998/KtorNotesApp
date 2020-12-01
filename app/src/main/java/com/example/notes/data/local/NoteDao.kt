fe67cdc48ed9e7f0fefbd6883a381fe48496eacf
Lubomir1998 <lubcho_sk8@abv.bg>
2020-12-01 20:43:37 +0200

    Initial commit and Room setup

---
+++ app/src/main/java/com/example/notes/data/local/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.notes.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Query("DELETE FROM notes WHERE id = :noteId")
+    suspend fun deleteNoteById(noteId: String)
+
+    @Query("SELECT * FROM notes ORDER BY date DESC")
+    fun getAllNotes(): Flow<List<Note>>
+
+
+
+}
---
+++ app/src/main/java/com/example/notes/data/local/NoteDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.notes.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Note::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+}
