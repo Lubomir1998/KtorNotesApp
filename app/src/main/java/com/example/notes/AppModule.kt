1b9ee7f1c774d0b8fa2fb589a66c2ffb5980f2c8
Lubomir1998 <lubcho_sk8@abv.bg>
2020-12-02 01:48:48 +0200

    DaggerHilt setup

---
+++ app/src/main/java/com/example/notes/AppModule.kt
@@ -0,0 +1,80 @@
+package com.example.notes
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.example.notes.data.local.NoteDatabase
+import com.example.notes.data.remote.BasicAuthInterceptor
+import com.example.notes.data.remote.NoteApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+
+    @Singleton
+    @Provides
+    fun provideNoteDB(@ApplicationContext context: Context): RoomDatabase = Room.databaseBuilder(
+        context,
+        NoteDatabase::class.java,
+        "notesDB"
+    ).fallbackToDestructiveMigrationOnDowngrade()
+        .build()
+
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(db: NoteDatabase) = db.noteDao()
+
+
+    @Singleton
+    @Provides
+    fun provideBaseInterceptor() = BasicAuthInterceptor()
+
+
+    @Singleton
+    @Provides
+    fun provideNoteApi(interceptor: BasicAuthInterceptor): NoteApi {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl("http://192.168.0.102:8080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(NoteApi::class.java)
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideEncryptedSharedPrefs(@ApplicationContext context: Context): SharedPreferences {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            context,
+            "encryptedSharedPrefsName",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+
+}
