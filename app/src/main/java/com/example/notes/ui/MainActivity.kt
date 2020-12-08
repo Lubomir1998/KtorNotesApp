0e2269f822e32a2dab5c0bdf951ebe98636d368f
Lubomir1998 <lubcho_sk8@abv.bg>
2020-12-09 00:05:32 +0200

    Actionbar background color setup

--- app/src/main/java/com/example/notes/ui/MainActivity.kt
+++ app/src/main/java/com/example/notes/ui/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.notes.ui
 
-import android.annotation.SuppressLint
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notes.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,28 +8,12 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceAsColor")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Define ActionBar object
-        // Define ActionBar object
-        val actionBar: ActionBar? = supportActionBar
-
-        // Define ColorDrawable object and parse color
-        // using parseColor method
-        // with color hash code as its parameter
-
-        // Define ColorDrawable object and parse color
-        // using parseColor method
-        // with color hash code as its parameter
-        val colorDrawable = ColorDrawable(R.color.black)
-
-        // Set BackgroundDrawable
-
-        // Set BackgroundDrawable
-        actionBar?.setBackgroundDrawable(colorDrawable)
 
     }
 }
